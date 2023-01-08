2396df530b20e33ce56a3ea5a2b406bc061067bd
Daniel Figueroa <da.figuepa@gmail.com>
2023-01-08 16:04:27 -0500

    Constructors

--- OOP/src.cpp
+++ OOP/src.cpp
@@ -13,15 +13,23 @@ public:
 		cout << "Company - " << company << endl;
 		cout << "Age - " << age << endl;
 	}
+	Employee(string cName, string cCompany, int cAge) {
+		name = cName;
+		company = cCompany;
+		age = cAge;
+	}
 };
 
 int main() {
 
-	Employee employee1;
-	employee1.name = "Daniel";
-	employee1.company = "Apple";
-	employee1.age = 30;
+	Employee employee1 = Employee("Daniel", "Apple", 30);
+	//employee1.name = "Daniel";
+	//employee1.company = "Apple";
+	//employee1.age = 30;
 	employee1.introduceYourself();
+
+	Employee employee2 = Employee("Romanet", "Google", 33);
+	employee2.introduceYourself();
 
 	return 0;
 }
